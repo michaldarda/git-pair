640160108cf2041d72a74bd03520f456467d311c
Michał Darda <michaldarda@gmail.com>
2025-09-09 21:36:28 +0200

    Handle git pair init basic version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() > 1 && args[1] == "init" {
+        match env::current_dir() {
+            Ok(path) => println!("{}", path.display()),
+            Err(e) => eprintln!("Error getting current directory: {}", e),
+        }
+    } else {
+        println!("Hello, world!");
+    }
 }
